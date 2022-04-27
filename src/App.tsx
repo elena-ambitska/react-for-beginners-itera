ab0f26587cbe8dc9d20c56f4e774ed0caf5b11dc
Lena <lambitska92@gmail.com>
2022-04-27 21:49:12 +0300

    Task done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import myJSON from "./my.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hi! My name is {myJSON.name}</h1>
+        <p>I am from {myJSON.bio.city}. I am {myJSON.bio.year}. My stack:</p>
+<ul>
+    {myJSON.bio.stack.map((item, index) => {
+        return <li key={index}>{item}</li>
+    })}
+</ul>
+        <a href={myJSON.contact}>My Linkedin</a>
     </div>
   );
 }
